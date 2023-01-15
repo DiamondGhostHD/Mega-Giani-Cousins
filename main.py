96fad5f16a8668b4739d10b539dd861abab7bf09
DiamondGhostHD <91199310+DiamondGhostHD@users.noreply.github.com>
2023-01-15 10:20:31 +0200

    Update main.py
    Setting up the display

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-import pygame, sys, os
+import pygame
 
+pygame.init()
+pygame.font.init()
+
+
+# display resolution
+edge_left = 0
+edge_right = 1600
+edge_top = 0
+edge_bottom = 900
+screen = pygame.display.set_mode((edge_right, edge_bottom))
+
+
+# display color
+color_display = (140, 217, 7)
+screen.fill(color_display)
+
+
+running = True
+while running:
+
+    screen.fill(color_display)  # erease what's been before
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                running = False
+
+    # refreshing the display
+    pygame.display.update()
+
+
+pygame.quit()
