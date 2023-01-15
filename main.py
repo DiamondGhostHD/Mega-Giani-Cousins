82556db79ef00e5abf84c7be32aca44f1af8d896
DiamondGhostHD <91199310+DiamondGhostHD@users.noreply.github.com>
2023-01-15 12:39:45 +0200

    Images + code
    - added background image
    - added Giani (character image)
    - infinite screen
    - some game logic

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pygame.locals import *
 pygame.init()
 
 
-
 # display resolution
 edge_left = 0
 edge_right = 1600
@@ -11,23 +10,55 @@ edge_top = 0
 edge_bottom = 900
 screen = pygame.display.set_mode((edge_right, edge_bottom))
 
+#game name
+pygame.display.set_caption('Mega Giani Cousins')
+
 
 # backround image	
-bg_img = pygame.image.load('C:\School\Meditatii\Info')
+bg_img = pygame.image.load('./bg.jpeg')
 bg_img = pygame.transform.scale(bg_img,(edge_right,edge_bottom))
+i = 0
 
+# Giani
+big_giani = pygame.image.load('./giani.png')                                           #? load the image from the files 
+big_giani_size = big_giani.get_size()                                                  #? get the size of the original image
+giani = pygame.transform.scale(big_giani, (big_giani_size[0]/8, big_giani_size[1]/8))  #? resize the image because it was too big
+pos_x = 50
+pos_y = 600
 
-running = True
-while running:
+#gamplay
+gravity = 1
+ 
 
+runing = True
+while runing:
+
+    #infinite display
+    screen.blit(bg_img, (i,0))
+    screen.blit(bg_img, (edge_right + i, 0))
+    if (i == -edge_right):
+        screen.blit(bg_img, (edge_right + i, 0))
+        i = 0
+    i -= 1
+
+    #show Giani on screen
+    screen.blit(giani, (pos_x, pos_y))
 
     for event in pygame.event.get():
+
+        #movement
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                running = False
+                change_to = "up"
 
-    # refreshing the display
+        #close the game
+        if event.type == QUIT:
+            runing = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                runing = False
+
     pygame.display.update()
-
 
 pygame.quit()
